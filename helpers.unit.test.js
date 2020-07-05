e60c7b7506058a031b04f8816216ab1ff6921fa8
George Karametas <georgios.karametas@viooh.com>
2020-07-05 15:35:28 +0100

    test(helpers): improve mocking

--- helpers.unit.test.js
+++ helpers.unit.test.js
@@ -1,9 +1,17 @@
 const { today } = require('./helpers')
 
-jest.mock('moment', () => () => ({ format: () => '30/01/2020' }))
+jest.mock('moment', () => () =>
+  jest.requireActual('moment')('2020-01-01T00:00:00.000Z'),
+)
 
-describe('today', () => {
-  it("should provide the today's date in a nice format", () => {
-    expect(today()).toBe('30/01/2020')
+describe('Helpers', () => {
+  afterEach(() => {
+    jest.resetModules()
+  })
+
+  describe('today', () => {
+    it("should provide the today's date in a nice format", () => {
+      expect(today()).toBe('01-01-2020')
+    })
   })
 })
