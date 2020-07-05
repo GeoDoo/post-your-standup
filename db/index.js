97158ddc8a3609ec3792678e3abfbf62e5ec552c
elefher <eldelielev@gmail.com>
2020-07-05 21:43:09 +0100

    chore(rebase): rebase

--- db/index.js
+++ db/index.js
@@ -1,18 +1,22 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
 const connect = () => {
-    mongoose.connect(`mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`, {
-    // mongoose.connect(`mongodb://standup:lefteris@localhost:27017`, {
+  mongoose
+    .connect(
+      `mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
+      {
+        // mongoose.connect(`mongodb://standup:lefteris@localhost:27017`, {
         useNewUrlParser: true,
-        useUnifiedTopology: true
-    })
-        .then(() => console.log('connection succesful'))
-        .catch((err) => console.error(err));
-};
-
-const getConnection = () => {
-    connect();
-    return mongoose.connection;
+        useUnifiedTopology: true,
+      },
+    )
+    .then(() => console.log('connection succesful'))
+    .catch(err => console.error(err))
 }
 
-module.exports = {getConnection};
+const getConnection = () => {
+  connect()
+  return mongoose.connection
+}
+
+module.exports = { getConnection }
--- db/models/Workspaces.js
+++ db/models/Workspaces.js
@@ -1,11 +1,11 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
-const Schema = mongoose.Schema;
+const Schema = mongoose.Schema
 
 const WorkspacesSchema = new Schema({
-    teamId: String,
-    teamDomain: String,
-});
+  teamId: String,
+  teamDomain: String,
+})
 
-const WorkspacesModel = mongoose.model('WorkspacesModel', WorkspacesSchema);
-module.exports = WorkspacesModel;
+const WorkspacesModel = mongoose.model('WorkspacesModel', WorkspacesSchema)
+module.exports = WorkspacesModel
