0a52a38b3655fd1bdcee348f566b5f8862469add
George Karametas <georgios.karametas@viooh.com>
2020-07-05 15:53:15 +0100

    test: clearer dates

--- helpers.unit.test.js
+++ helpers.unit.test.js
@@ -1,7 +1,7 @@
 const { today } = require('./helpers')
 
 jest.mock('moment', () => () =>
-  jest.requireActual('moment')('2020-01-01T00:00:00.000Z'),
+  jest.requireActual('moment')('2020-01-31T00:00:00.000Z'),
 )
 
 describe('Helpers', () => {
@@ -11,7 +11,7 @@ describe('Helpers', () => {
 
   describe('today', () => {
     it("should provide the today's date in a nice format", () => {
-      expect(today()).toBe('01-01-2020')
+      expect(today()).toBe('31-01-2020')
     })
   })
 })
