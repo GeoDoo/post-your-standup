4ece18b89edd40358e0e0fea5f1bd11ea2983ce1
George Karametas <georgios.karametas@viooh.com>
2020-08-13 22:52:40 +0100

    test(coverage): increase coverage threshold

--- jest.config.js
+++ jest.config.js
@@ -2,10 +2,10 @@ module.exports = {
   verbose: true,
   coverageThreshold: {
     global: {
-      branches: 0,
-      functions: 10,
-      lines: 10,
-      statements: 10,
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
     },
   },
 }
