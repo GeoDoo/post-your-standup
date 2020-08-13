c3c562cb82d5575658f328bfe34c97cef544764d
George Karametas <georgios.karametas@viooh.com>
2020-08-13 22:38:33 +0100

    test(staticselect): add missing case for optional initial option

--- core/blocks/staticSelect.unit.test.js
+++ core/blocks/staticSelect.unit.test.js
@@ -15,6 +15,43 @@ describe('Static select block', () => {
           value: 'Option one',
         },
       ],
+    )
+
+    expect(staticSelectBlock).toEqual({
+      type: 'static_select',
+      action_id: 'some:action',
+      placeholder: {
+        type: 'plain_text',
+        text: 'Select a channel',
+        emoji: true,
+      },
+      options: [
+        {
+          text: {
+            type: 'plain_text',
+            text: 'Option one',
+            emoji: true,
+          },
+          value: 'Option one',
+        },
+      ],
+    })
+  })
+
+  it('should return a block of type static_select with initial option if provided', () => {
+    const staticSelectBlock = getStaticSelectBlock(
+      'some:action',
+      'Select a channel',
+      [
+        {
+          text: {
+            type: 'plain_text',
+            text: 'Option one',
+            emoji: true,
+          },
+          value: 'Option one',
+        },
+      ],
       'some_value',
       'Initial option',
     )
