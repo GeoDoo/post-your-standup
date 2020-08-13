9615ea66b35ed103e91a91fe5fac10fbe33c860d
George Karametas <georgios.karametas@viooh.com>
2020-08-13 22:33:09 +0100

    test(formatters): add missing test for formatIssues fuction

--- utils/formatters.unit.test.js
+++ utils/formatters.unit.test.js
@@ -1,4 +1,4 @@
-const { today } = require('./formatters')
+const { today, formatIssues } = require('./formatters')
 
 jest.mock('moment', () => () =>
   jest.requireActual('moment')('2020-01-31T00:00:00.000Z'),
@@ -12,6 +12,29 @@ describe('Helpers', () => {
   describe('today', () => {
     it("should provide the today's date in a nice format", () => {
       expect(today()).toBe('31-01-2020')
+    })
+  })
+
+  describe('formatIssues', () => {
+    it('should provide standup issues in a nice format', () => {
+      const data = [
+        {
+          key: 'key',
+          fields: {
+            summary: 'summary',
+            status: 'status',
+            updated: 10021230120,
+          },
+        },
+      ]
+
+      expect(formatIssues(data)).toMatchSnapshot()
+    })
+
+    it('should provide useful feedback when no issues found', () => {
+      const data = []
+
+      expect(formatIssues(data)).toBe('No issues found. All done here!')
     })
   })
 })
