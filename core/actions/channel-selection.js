ddd18217574c69c4d8b7dacdcc0cf79c925d4abb
George Karametas <georgios.karametas@viooh.com>
2020-08-17 20:12:13 +0100

    chore(minor cleanup): cleanup channel selection action

--- core/actions/channel-selection.js
+++ core/actions/channel-selection.js
@@ -1,30 +1,9 @@
 module.exports = app => async ({ ack, body, context }) => {
   ack()
 
-  console.log('channel:selection', body)
-
   // const selectedProject = body.actions[0].selected_option.value; // save to DB
 
   try {
-    // // Update the message
-    // const result = await app.client.chat.update({
-    //   token: context.botToken,
-    //   // ts of message to update
-    //   ts: body.message.ts,
-    //   // Channel of message
-    //   channel: body.channel.id,
-    //   blocks: [
-    //     {
-    //       type: 'section',
-    //       text: {
-    //         type: 'mrkdwn',
-    //         text: '*The button was clicked!*'
-    //       }
-    //     }
-    //   ],
-    //   text: 'Message from Test App'
-    // });
-    // console.log(result);
   } catch (error) {
     console.error(error)
   }
