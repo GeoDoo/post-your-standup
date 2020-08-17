51ef7a02390fc879aaf05bc3454775ec8b4cc949
George Karametas <georgios.karametas@viooh.com>
2020-08-17 17:06:00 +0100

    test(moment): mock moment in a better way

--- utils/formatters.unit.test.js
+++ utils/formatters.unit.test.js
@@ -1,14 +1,11 @@
 const { today, formatIssues } = require('./formatters')
 
-jest.mock('moment', () => () =>
-  jest.requireActual('moment')('2020-01-31T00:00:00.000Z'),
-)
+jest.mock('moment', () => () => ({
+  format: () => '31-01-2020',
+  fromNow: () => '6 months ago',
+}))
 
 describe('Helpers', () => {
-  afterEach(() => {
-    jest.resetModules()
-  })
-
   describe('today', () => {
     it("should provide the today's date in a nice format", () => {
       expect(today()).toBe('31-01-2020')
