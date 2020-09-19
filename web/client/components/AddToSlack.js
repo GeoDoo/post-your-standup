79720b2675eb9f76e7dc7479967933418d9ca04c
George Karametas <georgios.karametas@viooh.com>
2020-09-20 00:13:32 +0100

    style(slack button): make it bigger

--- web/client/components/AddToSlack.js
+++ web/client/components/AddToSlack.js
@@ -25,8 +25,7 @@ const AddToSlack = () => {
     <a href={url}>
       <img
         alt="Add to Slack"
-        height="40"
-        width="140"
+        height="60"
         src="https://platform.slack-edge.com/img/add_to_slack.png"
         srcSet="https://platform.slack-edge.com/img/add_to_slack.png 1x,
         https://platform.slack-edge.com/img/add_to_slack@2x.png 2x"
