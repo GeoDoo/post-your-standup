b50a42d83f08604ac0c68e09d81cedba5cf8cc63
George Karametas <georgios.karametas@viooh.com>
2020-09-20 14:58:48 +0100

    refactor(landing): add first step with photo

--- web/client/components/Features.js
+++ web/client/components/Features.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Features = () => {
   return (
     <div id="features" className="section">
-      <h3 className="section-title">Features</h3>
+      <h2 className="section-title">Features</h2>
     </div>
   )
 }
--- web/client/components/HowItWorks.js
+++ web/client/components/HowItWorks.js
@@ -1,9 +1,26 @@
 import React from 'react'
 
+import installApp from '../assets/images/install.png'
+
+import './HowItWorks.pcss'
+
 const HowItWorks = () => {
   return (
     <div id="how-it-works" className="section">
-      <h3 className="section-title">How it works</h3>
+      <h2 className="section-title">How it works</h2>
+      <ul>
+        <li>
+          <div className="full">
+            <div className="half">
+              <span className="step">#1</span>
+              <h3>Install Post Your Standup to your workspace</h3>
+            </div>
+            <div className="half">
+              <img src={installApp} />
+            </div>
+          </div>
+        </li>
+      </ul>
     </div>
   )
 }
