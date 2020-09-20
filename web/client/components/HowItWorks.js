88a7d455b8975dbaf2cee3369b79df68b71c76b3
George Karametas <georgios.karametas@viooh.com>
2020-09-20 15:36:35 +0100

    refactor(landing): add second step

--- web/client/components/HowItWorks.js
+++ web/client/components/HowItWorks.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import installApp from '../assets/images/install.png'
+import connectToJira from '../assets/images/connect-to-jira.png'
 
 import './HowItWorks.pcss'
 
@@ -14,9 +15,26 @@ const HowItWorks = () => {
             <div className="half">
               <span className="step">#1</span>
               <h3>Install Post Your Standup to your workspace</h3>
+              <p>Simply click on one of the Add to Slack buttons,</p>
+              <p>then choose the channel you want to integrate it with,</p>
+              <p>and finally the Allow button!</p>
             </div>
             <div className="half">
               <img src={installApp} />
+            </div>
+          </div>
+        </li>
+        <li>
+          <div className="full">
+            <div className="half">
+              <img src={connectToJira} />
+            </div>
+            <div className="half">
+              <span className="step">#2</span>
+              <h3>Connect to Jira</h3>
+              <p>Fill in the appropriate fields,</p>
+              <p>so the app can be authenticated to access your boards,</p>
+              <p>and hop in to the channel you integrated the app!</p>
             </div>
           </div>
         </li>
